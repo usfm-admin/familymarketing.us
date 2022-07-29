06be89a3283aef0f2bd3b14965c0948ce83896ad
usfm-admin <admin@familymarketing.us>
2022-07-28 19:32:44 -0700

    Comon

--- src/pages/services/logos-seals.js
+++ src/pages/services/logos-seals.js
@@ -2,29 +2,35 @@ import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import logo from "../../assets/unitedstatesfamilymarketing.webp"
 
-const title = 'Logo Design'
+import jsondata from "../../data/global.json"
+
+const title = 'Logo Design Services'
+
+const desc = jsondata.fullname +  " provides advanced " + title + " for businesses of all sizes."
 
 export default function WebDesign() {
-  <Helmet>
-  <meta charSet="utf-8" />
-  <title>{title} - Familymarketing.us</title>
-  <link rel="canonical" href="https://familymarketing.us" />
-  <meta
-    name="description"
-    content="United States Family Marketing offers affordable marketing services online."
-  />
-  <meta property="og:image" content={logo} />
-</Helmet>
-  return (
-    <div className="wdhero ">
-      <div className="bg-sky-500/50">
-        <div className=" p-12 text-3xl text-white">
-          United States Family Marketing provides advanced {title} services
-          for businesses of all sizes.
-        </div>
-        <Link className="m-10 border-x-2 p-2 text-white bg-blue-500 hover:bg-blue-400 border-black" to={'/contact'}>Contact</Link>
 
-      </div>      {/* Web Design */}
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title> {title + ' - ' + jsondata.shortdomain[0].toUpperCase() + jsondata.shortdomain.slice(1).toLowerCase()} </title>
+        <link rel="canonical" href="https://familymarketing.us/services/business-cards" />
+        <meta
+          name="description"
+          content={desc}
+        />
+        <meta property="og:image" content={logo} />
+      </Helmet>
+      <div className="wdhero ">
+        <div className="bg-sky-500/50">
+          <div className=" p-12 text-3xl text-white">
+            {desc}
+          </div>
+          <Link className="m-10 border-x-2 p-2 text-white bg-blue-500 hover:bg-blue-400 border-black" to={'/contact'}>Contact</Link>
+
+        </div>      {/* Web Design */}
+      </div>
     </div>
   );
 }
