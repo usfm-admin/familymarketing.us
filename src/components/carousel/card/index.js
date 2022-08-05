b1fadcf4a8ae43474631e10f968e285f24e89dfb
usfm-templates <usfm.templates@protonmail.com>
2022-08-05 01:21:37 -0700

    hover border

--- src/components/carousel/card/index.js
+++ src/components/carousel/card/index.js
@@ -1,6 +1,6 @@
 export default function Card(props) {
   return (
-    <div className="border shadow-sm">
+    <div className="transition hover:border-red-400 border-2 shadow-sm hover:scale-110">
       <div className="relative w-64 h-64 overflow-hidden">
         <img
           src={require(`../../../assets/${props.image}`)}
